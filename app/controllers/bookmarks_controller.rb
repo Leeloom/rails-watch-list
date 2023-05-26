44b5c79e93963b5ae06f7a726925771b0ac13d54
Manon Roussel <manonmsroussel@gmail.com>
2023-05-26 11:13:19 +0200

    partie1

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,18 @@
+class bookmarksController < ActionController::Base
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params[:bookmark])
+    @bookmark.save
+  end
